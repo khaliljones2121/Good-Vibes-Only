c928e7717e919f29f5ab1b3ec9b5781a5ecf450a
User Name <hymlast096@gmail.com>
2025-09-09 13:14:28 -0400

    goodvibes

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-// MongoDB connection
-const mongoURI = 'mongodb+srv://tsuworld:Mustapha0711..a@goodvibes.xapojqy.mongodb.net/?retryWrites=true&w=majority&appName=GoodVibes';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/your-db-name', { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('Connected to MongoDB'))
 	.catch((err) => console.error('MongoDB connection error:', err));
 
